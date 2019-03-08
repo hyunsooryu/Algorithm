a165b896276dc7b9e31865dd794e30806d2bbccd
hyunsooryu <blessdutch@naver.com>
2019-03-08 23:05:29 +0900

    Create BRUTE_FORCE_NTOM_Q3.cpp

---
+++ BRUTE_FORCE_NTOM_Q3.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+
+using namespace std;
+int N, M;
+
+map<vector<int>, bool> MAP;
+vector<int> path;
+void go(int i, int cnt) {
+	if (cnt == M) {
+		for (auto j : path) {
+			cout << j << " ";
+		}
+		cout << endl;
+		return;
+	}
+	for (int k = 1; k <= N; k++) {
+		path.push_back(k);
+		go(k, cnt + 1);
+		path.pop_back();
+	}
+}
+
+int main() {
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		path.push_back(i);
+		go(i, 1);
+		path.pop_back();
+	}
+
+	return 0;
+}
